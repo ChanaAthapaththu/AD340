d5e6954e1b5fa3cd407158b148769fb1d68ead03
ChanaAthapaththu <athapaththu.cc@gmail.com>
2021-08-30 11:52:06 +0530

    AD340 code new

--- app/src/main/java/com/example/ad340/ForeCastRepository.kt
+++ app/src/main/java/com/example/ad340/ForeCastRepository.kt
@@ -9,14 +9,25 @@ class ForeCastRepository {
     val weeklyForecast:LiveData<List<DailyForecast>> = _weeklyForecast
 
     fun loadForecast(zipcode: String){
-        val randomValues = List(7){ Random.nextFloat().rem(100)*100}
-        val forecastItems = randomValues.map {
-            DailyForecast(it,getTempDescription(it))
+        val randomValues = List(10){ Random.nextFloat().rem(100)*100}
+        val forecastItems = randomValues.map { temp->
+            DailyForecast(temp,getTempDescription(temp))
         }
         _weeklyForecast.setValue(forecastItems)
     }
     private fun getTempDescription(temp: Float):String{
-        return if(temp<75)"Its too cold" else "Its great"
+        return when(temp){
+            in Float.MIN_VALUE.rangeTo(0f) -> "Any thing below 0 doesn't make sense "
+            in 0f.rangeTo(32f)-> "Way too cold"
+            in 32f.rangeTo(55f)-> "Way too cold"
+            in 55f.rangeTo(65f)-> "Way too cold"
+            in 65f.rangeTo(80f)-> "Way too cold"
+            in 80f.rangeTo(90f)-> "Getting little cold"
+            in 90f.rangeTo(100f)-> "Where is the AC"
+            in 100f.rangeTo(Float.MAX_VALUE)-> "What is this Arizona?"
+            else -> "Does not compute"
+
+        }
     }
 
 }
